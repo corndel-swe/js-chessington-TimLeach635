1fd0cf8b91bc9f888fc546f138e6f478a590a83f
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2024-06-24 16:16:10 +0100

    Day 1 knight moves

--- src/models/pieces/knight.js
+++ src/models/pieces/knight.js
@@ -7,7 +7,45 @@ export default class Knight {
   }
 
   getAvailableMoves(board) {
-    return []
+    // get the square currently occupied by the knight
+    let location = board.findPiece(this)
+    
+    // the list of valid moves
+    let moves = []
+
+    if (location.row + 1 <= 7 && location.col + 2 <= 7) {
+      moves.push(new Square(location.row + 1, location.col + 2))
+    }
+
+    if (location.row + 1 <= 7 && location.col - 2 >= 0) {
+      moves.push(new Square(location.row + 1, location.col - 2))
+    }
+
+    if (location.row - 1 >= 0 && location.col + 2 <= 7) {
+      moves.push(new Square(location.row - 1, location.col + 2))
+    }
+
+    if (location.row - 1 >= 0 && location.col - 2 >= 0) {
+      moves.push(new Square(location.row - 1, location.col - 2))
+    }
+
+    if (location.row + 2 <= 7 && location.col + 1 <= 7) {
+      moves.push(new Square(location.row + 2, location.col + 1))
+    }
+
+    if (location.row + 2 <= 7 && location.col - 1 >= 0) {
+      moves.push(new Square(location.row + 2, location.col - 1))
+    }
+
+    if (location.row - 2 >= 0 && location.col + 1 <= 7) {
+      moves.push(new Square(location.row - 2, location.col + 1))
+    }
+
+    if (location.row - 2 >= 0 && location.col - 1 >= 0) {
+      moves.push(new Square(location.row - 2, location.col - 1))
+    }
+
+    return moves
   }
 
   moveTo(board, newSquare) {
