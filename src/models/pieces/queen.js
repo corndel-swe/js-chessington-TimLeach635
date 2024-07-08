b0f356519613e803511bbd74759067ab75e1c04b
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2024-07-08 11:07:07 +0100

    Implement queen movement

--- src/models/pieces/queen.js
+++ src/models/pieces/queen.js
@@ -7,7 +7,61 @@ export default class Queen {
   }
 
   getAvailableMoves(board) {
-    return []
+    // get the square currently occupied by the bishop
+    let location = board.findPiece(this)
+    
+    // the list of valid moves
+    let moves = []
+
+    for (let i = 1; i <= 7; i++) {
+      if (location.row + i <= 7) {
+        moves.push(new Square(location.row + i, location.col))
+      }
+    }
+
+    for (let i = 1; i <= 7; i++) {
+      if (location.row - i >= 0) {
+        moves.push(new Square(location.row - i, location.col))
+      }
+    }
+
+    for (let i = 1; i <= 7; i++) {
+      if (location.col + i <= 7) {
+        moves.push(new Square(location.row, location.col + i))
+      }
+    }
+
+    for (let i = 1; i <= 7; i++) {
+      if (location.col - i >= 0) {
+        moves.push(new Square(location.row, location.col - i))
+      }
+    }
+
+    for (let i = 1; i <= 7; i++) {
+      if (location.row + i <= 7 && location.col + i <= 7) {
+        moves.push(new Square(location.row + i, location.col + i))
+      }
+    }
+
+    for (let i = 1; i <= 7; i++) {
+      if (location.row - i >= 0 && location.col + i <= 7) {
+        moves.push(new Square(location.row - i, location.col + i))
+      }
+    }
+
+    for (let i = 1; i <= 7; i++) {
+      if (location.row - i >= 0 && location.col - i >= 0) {
+        moves.push(new Square(location.row - i, location.col - i))
+      }
+    }
+
+    for (let i = 1; i <= 7; i++) {
+      if (location.row + i <= 7 && location.col - i >= 0) {
+        moves.push(new Square(location.row + i, location.col - i))
+      }
+    }
+
+    return moves
   }
 
   moveTo(board, newSquare) {
